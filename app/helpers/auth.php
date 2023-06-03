7e85de19a055382a029fac090e43af03540d7a4b
MartMbithi <martdevelopers254@gmail.com>
2023-06-03 15:22:24 +0300

    Implement Core Application Helpers

--- app/helpers/auth.php
+++ app/helpers/auth.php
@@ -91,46 +91,6 @@ if (isset($_POST['Login'])) {
 }
 
 /* Register */
-if (isset($_POST['Register'])) {
-    $blood_donor_names = mysqli_real_escape_string($mysqli, $_POST['blood_donor_names']);
-    $blood_donor_contacts = mysqli_real_escape_string($mysqli, $_POST['blood_donor_contacts']);
-    $blood_donor_email = mysqli_real_escape_string($mysqli, $_POST['blood_donor_email']);
-    $blood_donor_address = mysqli_real_escape_string($mysqli, $_POST['blood_donor_address']);
-    $blood_donor_blood_group = mysqli_real_escape_string($mysqli, $_POST['blood_donor_blood_group']);
-    $login_password = sha1(md5(mysqli_real_escape_string($mysqli, $_POST['login_password'])));
-    $login_username = mysqli_real_escape_string($mysqli, $_POST['blood_donor_email']);
-    $login_rank = mysqli_real_escape_string($mysqli, 'Donor');
-
-    /* Prevent Duplications */
-    $duplication_checker = "SELECT * FROM login WHERE login_username = '{$login_username}'";
-    $res = mysqli_query($mysqli, $duplication_checker);
-    if (mysqli_num_rows($res) > 0) {
-        $err = "An account with this email already exists";
-    } else {
-        /* Persist */
-        $add_donor_auth = "INSERT INTO login (login_username, login_password, login_rank)
-        VALUES('{$login_username}', '{$login_password}', '{$login_rank}')";
-
-        if (mysqli_query($mysqli, $add_donor_auth)) {
-            /* Get Donor Login ID */
-            $donor_login_id = mysqli_real_escape_string($mysqli, mysqli_insert_id($mysqli));
-
-            /* Persist Donor */
-            $add_donor_sql = "INSERT INTO blood_donor (blood_donor_login_id, blood_donor_names, blood_donor_email, blood_donor_contacts, blood_donor_address, blood_donor_blood_group)
-            VALUES('{$donor_login_id}', '{$blood_donor_names}', '{$blood_donor_email}', '{$blood_donor_contacts}', '{$blood_donor_address}', '{$blood_donor_blood_group}')";
-
-            if (mysqli_query($mysqli, $add_donor_sql)) {
-                $_SESSION['success'] = "Account created successfully, proceed to login";
-                header('Location: login');
-                exit;
-            } else {
-                $err = "Failed, please try again";
-            }
-        } else {
-            $err = "Please try again";
-        }
-    }
-}
 
 /* Reset Password Step 1 */
 if (isset($_POST['Reset_Password_1'])) {
