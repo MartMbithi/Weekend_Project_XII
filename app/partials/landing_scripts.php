5a26bca71d6e113b3b52c779d7c92ef4ef42f6e2
MartMbithi <martdevelopers254@gmail.com>
2023-06-04 09:34:45 +0300

    Initial Theme Mitigation

---
+++ app/partials/landing_scripts.php
@@ -0,0 +1,21 @@
+<!-- Vendor JS -->
+<script src="../public/landing/js/vendor/jquery-3.5.1.min.js"></script>
+<script src="../public/landing/js/vendor/jquery.notify.min.js"></script>
+<script src="../public/landing/js/vendor/jquery.bundle.notify.min.js"></script>
+
+<script src="../public/landing/js/vendor/popper.min.js"></script>
+<script src="../public/landing/js/vendor/bootstrap.min.js"></script>
+<script src="../public/landing/js/vendor/jquery-migrate-3.3.0.min.js"></script>
+<script src="../public/landing/js/vendor/modernizr-3.11.2.min.js"></script>
+
+<!--Plugins JS-->
+<script src="../public/landing/js/plugins/swiper-bundle.min.js"></script>
+<script src="../public/landing/js/plugins/countdownTimer.min.js"></script>
+<script src="../public/landing/js/plugins/scrollup.js"></script>
+<script src="../public/landing/js/plugins/jquery.zoom.min.js"></script>
+<script src="../public/landing/js/plugins/slick.min.js"></script>
+<script src="../public/landing/js/plugins/infiniteslidev2.js"></script>
+<script src="../public/landing/js/vendor/jquery.magnific-popup.min.js"></script>
+<script src="../public/landing/js/plugins/jquery.sticky-sidebar.js"></script>
+<script src="../public/landing/js/vendor/index.js"></script>
+<script src="../public/landing/js/main.js"></script>
