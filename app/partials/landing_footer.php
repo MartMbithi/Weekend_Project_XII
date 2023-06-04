e4340428a7ecf1b65775cb169d65a2a362b1e0fe
MartMbithi <martdevelopers254@gmail.com>
2023-06-04 09:40:28 +0300

    Craft Index Pages

---
+++ app/partials/landing_footer.php
@@ -0,0 +1,29 @@
+<footer class="ec-footer section-space-mt">
+    <div class="footer-container">
+       
+        <div class="footer-bottom">
+            <div class="container">
+                <div class="row align-items-center">
+                    <!-- Footer social End -->
+                    <!-- Footer Copyright Start -->
+                    <div class="col text-center footer-copy">
+                        <div class="footer-bottom-copy ">
+                            <div class="ec-copy">Copyright © <?php echo date('Y'); ?> <a class="site-name text-upper" href="#">eFurniture Shop<span>.</span></a>. All Rights Reserved</div>
+                        </div>
+                    </div>
+                    <!-- Footer Copyright End -->
+                    <!-- Footer payment -->
+                    <div class="col footer-bottom-right">
+                        <div class="footer-bottom-payment d-flex justify-content-end">
+                            <div class="payment-link">
+                                <img src="../public/landing/images/icons/payment.png" alt="">
+                            </div>
+
+                        </div>
+                    </div>
+                    <!-- Footer payment -->
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
