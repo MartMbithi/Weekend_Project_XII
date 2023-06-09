29058c1a41cca59681aa8abb7035102d470144d8
MartMbithi <martdevelopers254@gmail.com>
2023-06-09 05:56:26 +0300

    Implement Categories - Implement Pull Functionality

--- ui/categories.php
+++ ui/categories.php
@@ -2,7 +2,7 @@
 session_start();
 require_once('../app/config/config.php');
 require_once('../app/config/checklogin.php');
-require_once('../app/helpers/users.php');
+require_once('../app/helpers/furniture.php');
 require_once('../app/partials/backoffice_head.php');
 ?>
 
@@ -25,11 +25,11 @@ require_once('../app/partials/backoffice_head.php');
                 <div class="col-sm-12">
                     <div class="page-title-box">
                         <div class="btn-group float-right m-t-15">
-                            <button type="button" data-toggle="modal" data-target="#add_modal" class="btn btn-custom waves-effect waves-light" aria-expanded="false">Add Customer
+                            <button type="button" data-toggle="modal" data-target="#add_modal" class="btn btn-custom waves-effect waves-light" aria-expanded="false">Add Category
                                 <span class="m-l-5"><i class="fa fa-plus"></i></span>
                             </button>
                         </div>
-                        <h4 class="page-title">Customers</h4>
+                        <h4 class="page-title">Furniture Categories</h4>
                     </div>
                 </div>
             </div>
@@ -39,7 +39,7 @@ require_once('../app/partials/backoffice_head.php');
                     <div class="modal-content">
                         <div class="modal-header align-items-center">
                             <div class="text-center">
-                                <h6 class="mb-0 text-bold">Register new customer</h6>
+                                <h6 class="mb-0 text-bold">Register new category</h6>
                             </div>
                             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                 <span aria-hidden="true">&times;</span>
@@ -48,37 +48,17 @@ require_once('../app/partials/backoffice_head.php');
                         <div class="modal-body">
                             <form class="needs-validation" method="post" enctype="multipart/form-data" role="form">
                                 <div class="row">
-                                    <div class="form-group col-md-6">
-                                        <label for="">First Name</label>
-                                        <input type="text" required name="customer_first_name" class="form-control">
-                                    </div>
-                                    <div class="form-group col-md-6">
-                                        <label for="">Last Name</label>
-                                        <input type="text" required name="customer_last_name" class="form-control">
-                                    </div>
-                                    <div class="form-group col-md-6">
-                                        <label for="">Contacts</label>
-                                        <input type="text" required name="customer_phone_number" class="form-control">
-                                    </div>
-                                    <div class="form-group col-md-6">
-                                        <label for="">Email</label>
-                                        <input type="email" required name="customer_email" class="form-control">
-                                    </div>
-                                    <div class="form-group col-md-6">
-                                        <label for="">Login Password</label>
-                                        <input type="password" required name="new_password" class="form-control">
-                                    </div>
-                                    <div class="form-group col-md-6">
-                                        <label for="">Confirm Password</label>
-                                        <input type="password" required name="confirm_password" class="form-control">
+                                    <div class="form-group col-md-12">
+                                        <label for="">Category Name</label>
+                                        <input type="text" required name="category_name" class="form-control">
                                     </div>
                                     <div class="form-group col-md-12">
-                                        <label for="">Address</label>
-                                        <input type="text" required name="customer_address" class="form-control">
+                                        <label for="">Category Description</label>
+                                        <textarea type="text" required name="category_description" class="form-control"></textarea>
                                     </div>
                                 </div>
                                 <div class="text-right">
-                                    <button type="submit" name="Add_Customer" class="btn btn-outline-danger">Save</button>
+                                    <button type="submit" name="Add_Category" class="btn btn-outline-danger">Save</button>
                                 </div>
                             </form>
                         </div>
@@ -93,21 +73,17 @@ require_once('../app/partials/backoffice_head.php');
                             <thead>
                                 <tr>
                                     <th>#</th>
-                                    <th>Names</th>
-                                    <th>Email</th>
-                                    <th>Phone Number</th>
-                                    <th>Address</th>
+                                    <th>Category Names</th>
+                                    <th>Category Details</th>
                                     <th>Manage</th>
                                 </tr>
                             </thead>
 
                             <tbody>
                                 <?php
-                                /* Pull Customer Details */
                                 $fetch_records_sql = mysqli_query(
                                     $mysqli,
-                                    "SELECT * FROM customer c
-                                    INNER JOIN login l ON l.login_id = c.customer_login_id"
+                                    "SELECT * FROM furniture_category"
                                 );
                                 $cnt = $cnt + 1;
                                 if (mysqli_num_rows($fetch_records_sql) > 0) {
@@ -115,18 +91,16 @@ require_once('../app/partials/backoffice_head.php');
                                 ?>
                                         <tr>
                                             <td><?php echo $cnt; ?></td>
-                                            <td><?php echo $rows['customer_first_name'] . ' ' . $rows['customer_last_name']; ?></td>
-                                            <td><?php echo $rows['customer_email']; ?></td>
-                                            <td><?php echo $rows['customer_phone_number']; ?></td>
-                                            <td><?php echo $rows['customer_address']; ?></td>
+                                            <td><?php echo $rows['category_name']; ?></td>
+                                            <td><?php echo $rows['category_description']; ?></td>
                                             <td>
-                                                <a data-toggle="modal" href="#update_<?php echo $rows['customer_id']; ?>" class="badge badge-pill badge-warning"><em class="fa fa-edit"></em> Edit</a>
-                                                <a data-toggle="modal" href="#delete_<?php echo $rows['customer_id']; ?>" class="badge badge-pill badge-danger"><em class="fa fa-trash"></em> Delete</a>
+                                                <a data-toggle="modal" href="#update_<?php echo $rows['category_id']; ?>" class="badge badge-pill badge-warning"><em class="fa fa-edit"></em> Edit</a>
+                                                <a data-toggle="modal" href="#delete_<?php echo $rows['category_id']; ?>" class="badge badge-pill badge-danger"><em class="fa fa-trash"></em> Delete</a>
                                             </td>
                                         </tr>
                                 <?php
                                         $cnt = $cnt + 1;
-                                        include('../app/modals/customers.php');
+                                        include('../app/modals/categories.php');
                                     }
                                 } ?>
                             </tbody>
