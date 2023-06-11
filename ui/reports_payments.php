60a8f6b834aafe2bbb30bb9e9094020fffa857c4
MartMbithi <martdevelopers254@gmail.com>
2023-06-11 04:57:53 +0300

    Implement Reports Payments

--- ui/reports_payments.php
+++ ui/reports_payments.php
@@ -31,39 +31,78 @@ require_once('../app/partials/backoffice_head.php');
             <div class="row">
                 <div class="col-lg-12 col-xl-12">
                     <div class="card-box">
-                        <table id="datatable-buttons" class="table table-striped table-bordered dt-responsive nowrap" style="border-collapse: collapse; border-spacing: 0; width: 100%;">
-                            <thead>
-                                <tr>
-                                    <th>Payment Ref Code</th>
-                                    <th>Order Number</th>
-                                    <th>Customer</th>
-                                    <th>Amount</th>
-                                    <th>Date Paid</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <?php
-                                $fetch_records_sql = mysqli_query(
-                                    $mysqli,
-                                    "SELECT * FROM payment p
+                        <?php if ($_SESSION['login_rank'] == 'Admin') { ?>
+                            <table id="datatable-buttons" class="table table-striped table-bordered dt-responsive nowrap" style="border-collapse: collapse; border-spacing: 0; width: 100%;">
+                                <thead>
+                                    <tr>
+                                        <th>Payment Ref Code</th>
+                                        <th>Order Number</th>
+                                        <th>Customer</th>
+                                        <th>Amount</th>
+                                        <th>Date Paid</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php
+                                    $fetch_records_sql = mysqli_query(
+                                        $mysqli,
+                                        "SELECT * FROM payment p
                                     INNER JOIN orders o ON o.order_id = p.payment_order_id
                                     INNER JOIN customer c ON c.customer_id = o.order_customer_id"
-                                );
-                                if (mysqli_num_rows($fetch_records_sql) > 0) {
-                                    while ($rows = mysqli_fetch_array($fetch_records_sql)) {
-                                ?>
-                                        <tr>
-                                            <td><?php echo $rows['payment_ref_code']; ?></td>
-                                            <td><?php echo $rows['order_ref_code']; ?></td>
-                                            <td><?php echo $rows['customer_first_name'] . ' ' . $rows['customer_last_name']; ?></td>
-                                            <td>Kes <?php echo number_format($rows['order_amount']); ?></td>
-                                            <td><?php echo $rows['payment_date']; ?></td>
-                                        </tr>
-                                <?php
-                                    }
-                                } ?>
-                            </tbody>
-                        </table>
+                                    );
+                                    if (mysqli_num_rows($fetch_records_sql) > 0) {
+                                        while ($rows = mysqli_fetch_array($fetch_records_sql)) {
+                                    ?>
+                                            <tr>
+                                                <td><?php echo $rows['payment_ref_code']; ?></td>
+                                                <td><?php echo $rows['order_ref_code']; ?></td>
+                                                <td><?php echo $rows['customer_first_name'] . ' ' . $rows['customer_last_name']; ?></td>
+                                                <td>Kes <?php echo number_format($rows['order_amount']); ?></td>
+                                                <td><?php echo $rows['payment_date']; ?></td>
+                                            </tr>
+                                    <?php
+                                        }
+                                    } ?>
+                                </tbody>
+                            </table>
+                        <?php } else { ?>
+                            <table id="datatable-buttons" class="table table-striped table-bordered dt-responsive nowrap" style="border-collapse: collapse; border-spacing: 0; width: 100%;">
+                                <thead>
+                                    <tr>
+                                        <th>Payment Ref Code</th>
+                                        <th>Order Number</th>
+                                        <th>Customer</th>
+                                        <th>Amount</th>
+                                        <th>Date Paid</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php
+                                    $fetch_records_sql = mysqli_query(
+                                        $mysqli,
+                                        "SELECT * FROM payment p
+                                        INNER JOIN orders o ON o.order_id = p.payment_order_id
+                                        INNER JOIN furniture f ON f.furniture_id = o.order_furniture_id
+                                        INNER JOIN furniture_seller fs ON fs.seller_id = f.furniture_seller_id
+                                        INNER JOIN customer c ON c.customer_id = o.order_customer_id
+                                        WHERE f.furniture_seller_id = '{$_SESSION['login_id']}'"
+                                    );
+                                    if (mysqli_num_rows($fetch_records_sql) > 0) {
+                                        while ($rows = mysqli_fetch_array($fetch_records_sql)) {
+                                    ?>
+                                            <tr>
+                                                <td><?php echo $rows['payment_ref_code']; ?></td>
+                                                <td><?php echo $rows['order_ref_code']; ?></td>
+                                                <td><?php echo $rows['customer_first_name'] . ' ' . $rows['customer_last_name']; ?></td>
+                                                <td>Kes <?php echo number_format($rows['order_amount']); ?></td>
+                                                <td><?php echo $rows['payment_date']; ?></td>
+                                            </tr>
+                                    <?php
+                                        }
+                                    } ?>
+                                </tbody>
+                            </table>
+                        <?php } ?>
                     </div>
                 </div><!-- end col-->
             </div>
