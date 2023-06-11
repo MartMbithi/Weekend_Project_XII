3195176567a6f18e3fff0872ad4f94f0551e4704
MartMbithi <martdevelopers254@gmail.com>
2023-06-11 04:21:48 +0300

    Implement Top Navigation Menu

--- app/partials/backoffice_header.php
+++ app/partials/backoffice_header.php
@@ -1,115 +1,287 @@
-<header id="topnav">
-    <div class="topbar-main">
-        <div class="container">
+<?php
+/*
+ *   Crafted On Sun Jun 11 2023
+ *   Author Martin Mbithi (martin@devlan.co.ke)
+ *   
+ *   www.devlan.co.ke
+ *   hello@devlan.co.ke
+ *
+ *
+ *   The Devlan Solutions LTD End User License Agreement
+ *   Copyright (c) 2022 Devlan Solutions LTD
+ *
+ *
+ *   1. GRANT OF LICENSE 
+ *   Devlan Solutions LTD hereby grants to you (an individual) the revocable, personal, non-exclusive, and nontransferable right to
+ *   install and activate this system on two separated computers solely for your personal and non-commercial use,
+ *   unless you have purchased a commercial license from Devlan Solutions LTD. Sharing this Software with other individuals, 
+ *   or allowing other individuals to view the contents of this Software, is in violation of this license.
+ *   You may not make the Software available on a network, or in any way provide the Software to multiple users
+ *   unless you have first purchased at least a multi-user license from Devlan Solutions LTD.
+ *
+ *   2. COPYRIGHT 
+ *   The Software is owned by Devlan Solutions LTD and protected by copyright law and international copyright treaties. 
+ *   You may not remove or conceal any proprietary notices, labels or marks from the Software.
+ *
+ *
+ *   3. RESTRICTIONS ON USE
+ *   You may not, and you may not permit others to
+ *   (a) reverse engineer, decompile, decode, decrypt, disassemble, or in any way derive source code from, the Software;
+ *   (b) modify, distribute, or create derivative works of the Software;
+ *   (c) copy (other than one back-up copy), distribute, publicly display, transmit, sell, rent, lease or 
+ *   otherwise exploit the Software. 
+ *
+ *
+ *   4. TERM
+ *   This License is effective until terminated. 
+ *   You may terminate it at any time by destroying the Software, together with all copies thereof.
+ *   This License will also terminate if you fail to comply with any term or condition of this Agreement.
+ *   Upon such termination, you agree to destroy the Software, together with all copies thereof.
+ *
+ *
+ *   5. NO OTHER WARRANTIES. 
+ *   DEVLAN SOLUTIONS LTD  DOES NOT WARRANT THAT THE SOFTWARE IS ERROR FREE. 
+ *   DEVLAN SOLUTIONS LTD SOFTWARE DISCLAIMS ALL OTHER WARRANTIES WITH RESPECT TO THE SOFTWARE, 
+ *   EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO IMPLIED WARRANTIES OF MERCHANTABILITY, 
+ *   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT OF THIRD PARTY RIGHTS. 
+ *   SOME JURISDICTIONS DO NOT ALLOW THE EXCLUSION OF IMPLIED WARRANTIES OR LIMITATIONS
+ *   ON HOW LONG AN IMPLIED WARRANTY MAY LAST, OR THE EXCLUSION OR LIMITATION OF 
+ *   INCIDENTAL OR CONSEQUENTIAL DAMAGES,
+ *   SO THE ABOVE LIMITATIONS OR EXCLUSIONS MAY NOT APPLY TO YOU. 
+ *   THIS WARRANTY GIVES YOU SPECIFIC LEGAL RIGHTS AND YOU MAY ALSO 
+ *   HAVE OTHER RIGHTS WHICH VARY FROM JURISDICTION TO JURISDICTION.
+ *
+ *
+ *   6. SEVERABILITY
+ *   In the event of invalidity of any provision of this license, the parties agree that such invalidity shall not
+ *   affect the validity of the remaining portions of this license.
+ *
+ *
+ *   7. NO LIABILITY FOR CONSEQUENTIAL DAMAGES IN NO EVENT SHALL DEVLAN SOLUTIONS LTD OR ITS SUPPLIERS BE LIABLE TO YOU FOR ANY
+ *   CONSEQUENTIAL, SPECIAL, INCIDENTAL OR INDIRECT DAMAGES OF ANY KIND ARISING OUT OF THE DELIVERY, PERFORMANCE OR 
+ *   USE OF THE SOFTWARE, EVEN IF DEVLAN SOLUTIONS LTD HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES
+ *   IN NO EVENT WILL DEVLAN SOLUTIONS LTD  LIABILITY FOR ANY CLAIM, WHETHER IN CONTRACT 
+ *   TORT OR ANY OTHER THEORY OF LIABILITY, EXCEED THE LICENSE FEE PAID BY YOU, IF ANY.
+ *
+ */
+if ($_SESSION['login_rank'] == 'Admin') {
+?>
+    <header id="topnav">
+        <div class="topbar-main">
+            <div class="container">
 
-            <!-- LOGO -->
-            <div class="topbar-left">
-                <a href="dashboard" class="logo">
-                    <i class="zmdi zmdi-shopping-basket"></i>
-                    <span>e - Furniture Store</span>
-                </a>
-            </div>
-            <!-- End Logo container-->
+                <!-- LOGO -->
+                <div class="topbar-left">
+                    <a href="dashboard" class="logo">
+                        <i class="zmdi zmdi-shopping-basket"></i>
+                        <span>e - Furniture Store</span>
+                    </a>
+                </div>
+                <!-- End Logo container-->
 
 
-            <div class="menu-extras navbar-topbar">
+                <div class="menu-extras navbar-topbar">
 
-                <ul class="list-inline float-right mb-0">
+                    <ul class="list-inline float-right mb-0">
 
-                    <li class="list-inline-item">
-                        <!-- Mobile menu toggle-->
-                        <a class="navbar-toggle">
-                            <div class="lines">
-                                <span></span>
-                                <span></span>
-                                <span></span>
-                            </div>
-                        </a>
-                        <!-- End mobile menu toggle-->
-                    </li>
-
-
-                    <li class="list-inline-item dropdown notification-list">
-                        <a class="nav-link dropdown-toggle waves-effect waves-light nav-user" data-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
-                            <img src="../public/backoffice/images/users/avatar-1.jpg" alt="user" class="rounded-circle">
-                        </a>
-                        <div class="dropdown-menu dropdown-menu-right profile-dropdown " aria-labelledby="Preview">
-                            <!-- item-->
-                            <div class="dropdown-item noti-title">
-                                <h5 class="text-overflow"><small>Welcome !</small> </h5>
-                            </div>
-
-                            <!-- item-->
-                            <a href="profile" class="dropdown-item notify-item">
-                                <i class="zmdi zmdi-account-circle"></i> <span>Profile</span>
+                        <li class="list-inline-item">
+                            <!-- Mobile menu toggle-->
+                            <a class="navbar-toggle">
+                                <div class="lines">
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                </div>
                             </a>
+                            <!-- End mobile menu toggle-->
+                        </li>
 
-                            <!-- item-->
-                            <a href="logout" class="dropdown-item notify-item">
-                                <i class="zmdi zmdi-power"></i> <span>Logout</span>
+
+                        <li class="list-inline-item dropdown notification-list">
+                            <a class="nav-link dropdown-toggle waves-effect waves-light nav-user" data-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
+                                <img src="../public/backoffice/images/users/avatar-1.jpg" alt="user" class="rounded-circle">
                             </a>
+                            <div class="dropdown-menu dropdown-menu-right profile-dropdown " aria-labelledby="Preview">
+                                <!-- item-->
+                                <div class="dropdown-item noti-title">
+                                    <h5 class="text-overflow"><small>Welcome !</small> </h5>
+                                </div>
 
-                        </div>
-                    </li>
+                                <!-- item-->
+                                <a href="profile" class="dropdown-item notify-item">
+                                    <i class="zmdi zmdi-account-circle"></i> <span>Profile</span>
+                                </a>
 
-                </ul>
+                                <!-- item-->
+                                <a href="logout" class="dropdown-item notify-item">
+                                    <i class="zmdi zmdi-power"></i> <span>Logout</span>
+                                </a>
 
-            </div> <!-- end menu-extras -->
-            <div class="clearfix"></div>
+                            </div>
+                        </li>
 
-        </div> <!-- end container -->
-    </div>
-    <!-- end topbar-main -->
+                    </ul>
+
+                </div> <!-- end menu-extras -->
+                <div class="clearfix"></div>
+
+            </div> <!-- end container -->
+        </div>
+        <!-- end topbar-main -->
 
 
-    <div class="navbar-custom">
-        <div class="container">
-            <div id="navigation">
-                <!-- Navigation Menu-->
-                <ul class="navigation-menu">
-                    <li>
-                        <a href="dashboard"><i class="fa fa-home"></i> <span> Dashboard </span> </a>
-                    </li>
-                    <li class="has-submenu">
-                        <a href="#"><i class="fa fa-users"></i> <span> System Users </span> </a>
-                        <ul class="submenu megamenu">
-                            <li>
-                                <ul>
-                                    <li><a href="staffs">Staffs</a></li>
-                                    <li><a href="sellers">Furniture Sellers</a></li>
-                                    <li><a href="customers">Customers</a></li>
-                                </ul>
-                            </li>
-                        </ul>
-                    </li>
+        <div class="navbar-custom">
+            <div class="container">
+                <div id="navigation">
+                    <!-- Navigation Menu-->
+                    <ul class="navigation-menu">
+                        <li>
+                            <a href="dashboard"><i class="fa fa-home"></i> <span> Dashboard </span> </a>
+                        </li>
+                        <li class="has-submenu">
+                            <a href="#"><i class="fa fa-users"></i> <span> System Users </span> </a>
+                            <ul class="submenu megamenu">
+                                <li>
+                                    <ul>
+                                        <li><a href="staffs">Staffs</a></li>
+                                        <li><a href="sellers">Furniture Sellers</a></li>
+                                        <li><a href="customers">Customers</a></li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </li>
 
-                    <li class="has-submenu">
-                        <a href="#"><i class="fa fa-bed"></i> <span> Furnitures </span> </a>
-                        <ul class="submenu">
-                            <li><a href="categories">Categories</a></li>
-                            <li><a href="furnitures">Furnitures</a></li>
-                        </ul>
-                    </li>
-                    <li>
-                        <a href="orders"><i class="fa fa-shopping-cart"></i> <span> Orders </span> </a>
-                    </li>
-                    <li>
-                        <a href="payments"><i class="fa fa-money"></i> <span> Payments </span> </a>
-                    </li>
+                        <li class="has-submenu">
+                            <a href="#"><i class="fa fa-bed"></i> <span> Furnitures </span> </a>
+                            <ul class="submenu">
+                                <li><a href="categories">Categories</a></li>
+                                <li><a href="furnitures">Furnitures</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="orders"><i class="fa fa-shopping-cart"></i> <span> Orders </span> </a>
+                        </li>
+                        <li>
+                            <a href="payments"><i class="fa fa-money"></i> <span> Payments </span> </a>
+                        </li>
 
-                    <li class="has-submenu">
-                        <a href="#"><i class="zmdi zmdi-format-list-bulleted"></i> <span> Reports </span> </a>
-                        <ul class="submenu">
-                            <li><a href="reports_customers">Customers</a></li>
-                            <li><a href="reports_furniture_sellers">Furniture Sellers</a></li>
-                            <li><a href="reports_furniture">Furnitures</a></li>
-                            <li><a href="reports_orders">Orders</a></li>
-                            <li><a href="reports_payments">Payments</a></li>
-                        </ul>
-                    </li>
-                </ul>
-                <!-- End navigation menu  -->
+                        <li class="has-submenu">
+                            <a href="#"><i class="zmdi zmdi-format-list-bulleted"></i> <span> Reports </span> </a>
+                            <ul class="submenu">
+                                <li><a href="reports_customers">Customers</a></li>
+                                <li><a href="reports_furniture_sellers">Furniture Sellers</a></li>
+                                <li><a href="reports_furniture">Furnitures</a></li>
+                                <li><a href="reports_orders">Orders</a></li>
+                                <li><a href="reports_payments">Payments</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <!-- End navigation menu  -->
+                </div>
             </div>
         </div>
-    </div>
-</header>
+    </header>
+<?php } else { ?>
+    <header id="topnav">
+        <div class="topbar-main">
+            <div class="container">
+
+                <!-- LOGO -->
+                <div class="topbar-left">
+                    <a href="dashboard" class="logo">
+                        <i class="zmdi zmdi-shopping-basket"></i>
+                        <span>e - Furniture Store</span>
+                    </a>
+                </div>
+                <!-- End Logo container-->
+
+
+                <div class="menu-extras navbar-topbar">
+
+                    <ul class="list-inline float-right mb-0">
+
+                        <li class="list-inline-item">
+                            <!-- Mobile menu toggle-->
+                            <a class="navbar-toggle">
+                                <div class="lines">
+                                    <span></span>
+                                    <span></span>
+                                    <span></span>
+                                </div>
+                            </a>
+                            <!-- End mobile menu toggle-->
+                        </li>
+
+
+                        <li class="list-inline-item dropdown notification-list">
+                            <a class="nav-link dropdown-toggle waves-effect waves-light nav-user" data-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
+                                <img src="../public/backoffice/images/users/avatar-1.jpg" alt="user" class="rounded-circle">
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-right profile-dropdown " aria-labelledby="Preview">
+                                <!-- item-->
+                                <div class="dropdown-item noti-title">
+                                    <h5 class="text-overflow"><small>Welcome !</small> </h5>
+                                </div>
+
+                                <!-- item-->
+                                <a href="profile" class="dropdown-item notify-item">
+                                    <i class="zmdi zmdi-account-circle"></i> <span>Profile</span>
+                                </a>
+
+                                <!-- item-->
+                                <a href="logout" class="dropdown-item notify-item">
+                                    <i class="zmdi zmdi-power"></i> <span>Logout</span>
+                                </a>
+
+                            </div>
+                        </li>
+
+                    </ul>
+
+                </div> <!-- end menu-extras -->
+                <div class="clearfix"></div>
+
+            </div> <!-- end container -->
+        </div>
+        <!-- end topbar-main -->
+
+
+        <div class="navbar-custom">
+            <div class="container">
+                <div id="navigation">
+                    <!-- Navigation Menu-->
+                    <ul class="navigation-menu">
+                        <li>
+                            <a href="dashboard"><i class="fa fa-home"></i> <span> Dashboard </span> </a>
+                        </li>
+
+                        <li class="has-submenu">
+                            <a href="#"><i class="fa fa-bed"></i> <span> Furnitures </span> </a>
+                            <ul class="submenu">
+                                <li><a href="categories">Categories</a></li>
+                                <li><a href="furnitures">Furnitures</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="orders"><i class="fa fa-shopping-cart"></i> <span> Orders </span> </a>
+                        </li>
+                        <li>
+                            <a href="payments"><i class="fa fa-money"></i> <span> Payments </span> </a>
+                        </li>
+
+                        <li class="has-submenu">
+                            <a href="#"><i class="zmdi zmdi-format-list-bulleted"></i> <span> Reports </span> </a>
+                            <ul class="submenu">
+                                <li><a href="reports_furniture">Furnitures</a></li>
+                                <li><a href="reports_orders">Orders</a></li>
+                                <li><a href="reports_payments">Payments</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <!-- End navigation menu  -->
+                </div>
+            </div>
+        </div>
+    </header>
+<?php
+} ?>
