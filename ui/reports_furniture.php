47fa2f665c3c3676e76d05c92c88e87a73790980
MartMbithi <martdevelopers254@gmail.com>
2023-06-11 04:55:38 +0300

    Implement Reports Orders

--- ui/reports_furniture.php
+++ ui/reports_furniture.php
@@ -31,48 +31,94 @@ require_once('../app/partials/backoffice_head.php');
             <div class="row">
                 <div class="col-lg-12 col-xl-12">
                     <div class="card-box">
-                        <table id="datatable-buttons" class="table table-striped table-bordered dt-responsive nowrap" style="border-collapse: collapse; border-spacing: 0; width: 100%;">
-                            <thead>
-                                <tr>
-                                    <th>#</th>
-                                    <th>SKU</th>
-                                    <th>Category</th>
-                                    <th>Item Name</th>
-                                    <th>Seller</th>
-                                    <th>Unit price</th>
-                                    <th>Availability</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <?php
-                                $fetch_records_sql = mysqli_query(
-                                    $mysqli,
-                                    "SELECT * FROM furniture f
+                        <?php if ($_SESSION['login_rank'] == 'Admin') { ?>
+                            <table id="datatable-buttons" class="table table-striped table-bordered dt-responsive nowrap" style="border-collapse: collapse; border-spacing: 0; width: 100%;">
+                                <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>SKU</th>
+                                        <th>Category</th>
+                                        <th>Item Name</th>
+                                        <th>Seller</th>
+                                        <th>Unit price</th>
+                                        <th>Availability</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php
+                                    $fetch_records_sql = mysqli_query(
+                                        $mysqli,
+                                        "SELECT * FROM furniture f
                                     INNER JOIN furniture_category fc ON fc.category_id = f.furniture_category_id
                                     INNER JOIN furniture_seller fs ON fs.seller_id = f.furniture_seller_id
                                     "
-                                );
-                                $cnt = 1;
-                                if (mysqli_num_rows($fetch_records_sql) > 0) {
-                                    while ($rows = mysqli_fetch_array($fetch_records_sql)) {
-                                ?>
-                                        <tr>
-                                            <td><?php echo $cnt; ?></td>
-                                            <td><?php echo $rows['furniture_sku_code']; ?></td>
-                                            <td><?php echo $rows['category_name']; ?></td>
-                                            <td><?php echo $rows['furniture_name']; ?></td>
-                                            <td>
-                                                <?php echo $rows['seller_first_name'] . ' ' . $rows['seller_last_name']; ?>
-                                            </td>
-                                            <td>Kes <?php echo number_format($rows['furniture_price']); ?></td>
-                                            <td><?php echo $rows['furniture_status']; ?></td>
-                                        </tr>
-                                <?php
-                                        $cnt = $cnt + 1;
-                                    }
-                                } ?>
-                            </tbody>
-                        </table>
+                                    );
+                                    $cnt = 1;
+                                    if (mysqli_num_rows($fetch_records_sql) > 0) {
+                                        while ($rows = mysqli_fetch_array($fetch_records_sql)) {
+                                    ?>
+                                            <tr>
+                                                <td><?php echo $cnt; ?></td>
+                                                <td><?php echo $rows['furniture_sku_code']; ?></td>
+                                                <td><?php echo $rows['category_name']; ?></td>
+                                                <td><?php echo $rows['furniture_name']; ?></td>
+                                                <td>
+                                                    <?php echo $rows['seller_first_name'] . ' ' . $rows['seller_last_name']; ?>
+                                                </td>
+                                                <td>Kes <?php echo number_format($rows['furniture_price']); ?></td>
+                                                <td><?php echo $rows['furniture_status']; ?></td>
+                                            </tr>
+                                    <?php
+                                            $cnt = $cnt + 1;
+                                        }
+                                    } ?>
+                                </tbody>
+                            </table>
+                        <?php } else { ?>
+                            <table id="datatable-buttons" class="table table-striped table-bordered dt-responsive nowrap" style="border-collapse: collapse; border-spacing: 0; width: 100%;">
+                                <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>SKU</th>
+                                        <th>Category</th>
+                                        <th>Item Name</th>
+                                        <th>Seller</th>
+                                        <th>Unit price</th>
+                                        <th>Availability</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php
+                                    $fetch_records_sql = mysqli_query(
+                                        $mysqli,
+                                        "SELECT * FROM furniture f
+                                        INNER JOIN furniture_category fc ON fc.category_id = f.furniture_category_id
+                                        INNER JOIN furniture_seller fs ON fs.seller_id = f.furniture_seller_id
+                                        WHERE fs.seller_login_id = '{$_SESSION['login_id']}'
+                                        "
+                                    );
+                                    $cnt = 1;
+                                    if (mysqli_num_rows($fetch_records_sql) > 0) {
+                                        while ($rows = mysqli_fetch_array($fetch_records_sql)) {
+                                    ?>
+                                            <tr>
+                                                <td><?php echo $cnt; ?></td>
+                                                <td><?php echo $rows['furniture_sku_code']; ?></td>
+                                                <td><?php echo $rows['category_name']; ?></td>
+                                                <td><?php echo $rows['furniture_name']; ?></td>
+                                                <td>
+                                                    <?php echo $rows['seller_first_name'] . ' ' . $rows['seller_last_name']; ?>
+                                                </td>
+                                                <td>Kes <?php echo number_format($rows['furniture_price']); ?></td>
+                                                <td><?php echo $rows['furniture_status']; ?></td>
+                                            </tr>
+                                    <?php
+                                            $cnt = $cnt + 1;
+                                        }
+                                    } ?>
+                                </tbody>
+                            </table>
+                        <?php } ?>
                     </div>
                 </div><!-- end col-->
 
--- ui/reports_orders.php
+++ ui/reports_orders.php
@@ -31,46 +31,90 @@ require_once('../app/partials/backoffice_head.php');
             <div class="row">
                 <div class="col-lg-12 col-xl-12">
                     <div class="card-box">
-                        <table id="datatable-buttons" class="table table-striped table-bordered dt-responsive nowrap" style="border-collapse: collapse; border-spacing: 0; width: 100%;">
-                            <thead>
-                                <tr>
-                                    <th>#</th>
-                                    <th>Item</th>
-                                    <th>QTY</th>
-                                    <th>Customer</th>
-                                    <th>Seller</th>
-                                    <th>Cost</th>
-                                    <th>Payment Status</th>
-                                    <th>Delivery Status</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <?php
-                                /* Pull Orders */
-                                $fetch_records_sql = mysqli_query(
-                                    $mysqli,
-                                    "SELECT * FROM orders o INNER JOIN customer c ON c.customer_id = o.order_customer_id
+                        <?php if ($_SESSION['login_rank'] == 'Admin') { ?>
+                            <table id="datatable-buttons" class="table table-striped table-bordered dt-responsive nowrap" style="border-collapse: collapse; border-spacing: 0; width: 100%;">
+                                <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Item</th>
+                                        <th>QTY</th>
+                                        <th>Customer</th>
+                                        <th>Seller</th>
+                                        <th>Cost</th>
+                                        <th>Payment Status</th>
+                                        <th>Delivery Status</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php
+                                    /* Pull Orders */
+                                    $fetch_records_sql = mysqli_query(
+                                        $mysqli,
+                                        "SELECT * FROM orders o INNER JOIN customer c ON c.customer_id = o.order_customer_id
                                     INNER JOIN furniture f ON f.furniture_id = o.order_furniture_id
                                     INNER JOIN furniture_seller fs  ON fs.seller_id = f.furniture_seller_id"
-                                );
-                                if (mysqli_num_rows($fetch_records_sql) > 0) {
-                                    while ($rows = mysqli_fetch_array($fetch_records_sql)) {
-                                ?>
-                                        <tr>
-                                            <td><?php echo $rows['order_ref_code']; ?></td>
-                                            <td><?php echo $rows['furniture_name']; ?></td>
-                                            <td><?php echo $rows['order_qty']; ?></td>
-                                            <td><?php echo $rows['customer_first_name'] . ' ' . $rows['customer_last_name']; ?></td>
-                                            <td><?php echo $rows['seller_first_name'] . ' ' . $rows['seller_last_name']; ?></td>
-                                            <td>Kes <?php echo number_format($rows['order_amount']); ?></td>
-                                            <td><?php echo $rows['order_status']; ?></td>
-                                            <td><?php echo $rows['order_delivery_status']; ?></td>
-                                        </tr>
-                                <?php
-                                    }
-                                } ?>
-                            </tbody>
-                        </table>
+                                    );
+                                    if (mysqli_num_rows($fetch_records_sql) > 0) {
+                                        while ($rows = mysqli_fetch_array($fetch_records_sql)) {
+                                    ?>
+                                            <tr>
+                                                <td><?php echo $rows['order_ref_code']; ?></td>
+                                                <td><?php echo $rows['furniture_name']; ?></td>
+                                                <td><?php echo $rows['order_qty']; ?></td>
+                                                <td><?php echo $rows['customer_first_name'] . ' ' . $rows['customer_last_name']; ?></td>
+                                                <td><?php echo $rows['seller_first_name'] . ' ' . $rows['seller_last_name']; ?></td>
+                                                <td>Kes <?php echo number_format($rows['order_amount']); ?></td>
+                                                <td><?php echo $rows['order_status']; ?></td>
+                                                <td><?php echo $rows['order_delivery_status']; ?></td>
+                                            </tr>
+                                    <?php
+                                        }
+                                    } ?>
+                                </tbody>
+                            </table>
+                        <?php } else { ?>
+                            <table id="datatable-buttons" class="table table-striped table-bordered dt-responsive nowrap" style="border-collapse: collapse; border-spacing: 0; width: 100%;">
+                                <thead>
+                                    <tr>
+                                        <th>#</th>
+                                        <th>Item</th>
+                                        <th>QTY</th>
+                                        <th>Customer</th>
+                                        <th>Seller</th>
+                                        <th>Cost</th>
+                                        <th>Payment Status</th>
+                                        <th>Delivery Status</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php
+                                    /* Pull Orders */
+                                    $fetch_records_sql = mysqli_query(
+                                        $mysqli,
+                                        "SELECT * FROM orders o INNER JOIN customer c ON c.customer_id = o.order_customer_id
+                                        INNER JOIN furniture f ON f.furniture_id = o.order_furniture_id
+                                        INNER JOIN furniture_seller fs  ON fs.seller_id = f.furniture_seller_id
+                                        WHERE fs.seller_login_id = '{$_SESSION['login_id']}'"
+                                    );
+                                    if (mysqli_num_rows($fetch_records_sql) > 0) {
+                                        while ($rows = mysqli_fetch_array($fetch_records_sql)) {
+                                    ?>
+                                            <tr>
+                                                <td><?php echo $rows['order_ref_code']; ?></td>
+                                                <td><?php echo $rows['furniture_name']; ?></td>
+                                                <td><?php echo $rows['order_qty']; ?></td>
+                                                <td><?php echo $rows['customer_first_name'] . ' ' . $rows['customer_last_name']; ?></td>
+                                                <td><?php echo $rows['seller_first_name'] . ' ' . $rows['seller_last_name']; ?></td>
+                                                <td>Kes <?php echo number_format($rows['order_amount']); ?></td>
+                                                <td><?php echo $rows['order_status']; ?></td>
+                                                <td><?php echo $rows['order_delivery_status']; ?></td>
+                                            </tr>
+                                    <?php
+                                        }
+                                    } ?>
+                                </tbody>
+                            </table>
+                        <?php } ?>
                     </div>
                 </div><!-- end col-->
 
